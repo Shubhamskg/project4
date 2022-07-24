8f9b1631da34330382ccd9a405fcd0b7e9cbc69b
Shubhamskg <54672970+Shubhamskg@users.noreply.github.com>
2022-07-24 23:43:10 +0530

    Update and rename tic.js to index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
